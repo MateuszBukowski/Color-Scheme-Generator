fb942d6483efb52f829279c3b584b98a4768d59c
Mateusz <mateuszbukowski13@gmail.com>
2026-04-16 08:46:16 +0200

    Color API fetch ready
    Fetching and displaying color API ready

--- index.js
+++ index.js
@@ -2,22 +2,28 @@
 // - mode
 // - count=5
 // - rgb seed ? - color
+// Example:
+// "/scheme?hex=FF0&mode=monochrome&count=5"
 
 const color = document.getElementById('color')
 const mode = document.getElementById('mode')
-const count = 5
+const count = "5"
 
-fetch('https://www.thecolorapi.com/scheme?hex=FF0&mode=monochrome&count=5')
+let colorRequets = color.value.slice(1)
+
+let apiRequest = `https://www.thecolorapi.com/scheme?hex=${colorRequets}&mode=${mode.value}&count=${count}`
+console.log(apiRequest)
+
+fetch(apiRequest)
     .then(res => res.json())
     .then(data => getColorsHex(data.colors))
-
 
 function getColorsHex(dataColors){
         for (let index = 0; index < dataColors.length; index++)
             console.log(dataColors[index].hex.value)
 }
 
-console.log(`color: ${color.value}`)
+console.log(`color: ${colorRequets}`)
 console.log(`mode: ${mode.value}`)
 console.log(`count: ${count}`)
 
